cbeba79c3310f87586c8870e279870affc247ace
milaneyia <39941577+milaneyia@users.noreply.github.com>
2019-10-13 15:00:50 -0300

    title in rounds

--- app/models/rounds/Round.ts
+++ app/models/rounds/Round.ts
@@ -1,13 +1,28 @@
-import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { BaseEntity, Column, CreateDateColumn, Entity, LessThanOrEqual,
+    MoreThanOrEqual, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity()
 export class Round extends BaseEntity {
+
+    static findCurrentRound() {
+        const today = new Date();
+
+        return this.findOne({
+            where: {
+                submissionsEndedAt: MoreThanOrEqual(today),
+                submissionsStartedAt: LessThanOrEqual(today),
+            },
+        });
+    }
 
     @PrimaryGeneratedColumn()
     id!: number;
 
     @Column()
-    information!: string;
+    title!: string;
+
+    @Column({ nullable: true, length: 3000 })
+    information?: string;
 
     @Column('date')
     submissionsStartedAt!: Date;
