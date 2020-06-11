a83f187cb10ad47603aff0085ca3561052b5a8fc
milaneyia <39941577+milaneyia@users.noreply.github.com>
2020-06-11 02:11:13 -0400

    initial request

--- src/views/Judging.vue
+++ src/views/Judging.vue
@@ -17,13 +17,17 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import Axios from 'axios';
-
 import { State } from 'vuex-class';
 import Component from 'vue-class-component';
 import { Criteria, Round } from '../interfaces';
 import Qualifier from '../components/judging/QualifierJudging.vue';
 import Elimination from '../components/judging/EliminationJudging.vue';
+
+interface ApiResponse {
+    currentRound: Round | null;
+    criterias: Criteria[];
+    judgingDone: [];
+}
 
 @Component({
     components: {
@@ -39,10 +43,11 @@ export default class Judging extends Vue {
     judgingDone = [];
 
     async created (): Promise<void> {
-        const { data } = await Axios.get('/api/judging');
-        this.currentRound = data.currentRound;
-        this.criterias = data.criterias;
-        this.judgingDone = data.judgingDone;
+        await this.initialRequest<ApiResponse>('/api/judging', (data) => {
+            this.currentRound = data.currentRound;
+            this.criterias = data.criterias;
+            this.judgingDone = data.judgingDone;
+        });
     }
 }
 </script>
